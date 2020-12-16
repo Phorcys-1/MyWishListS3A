1b952b72575c4619658b8408b4ab7223017fb2e3
Jules <jfrancois968@gmail.com>
2020-12-16 14:57:09 +0100

    ctrl

---
+++ src/controleur/ItemControleur.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace wishlist\controleur;
+
+use wishlist\modele\Liste as Lst;
+use wishlist\vue\Liste as vLst;
+use wishlist\modele\Utilisateur as Utl;
+use wishlist\modele\Item as Itm;
+use wishlist\vue\Item as vLst;
+
+
+
+class ItemControleur {
+    protected $liste;
+
+    public function __construct($pName){
+        $this->liste=Lst
+    }
+}
