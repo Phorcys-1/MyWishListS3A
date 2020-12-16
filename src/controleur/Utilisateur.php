5d0baf06ea15c38c89f18079a08114a2fd12966f
Simon Hung <simonhung@hotmail.fr>
2020-12-16 15:06:34 +0100

    Ajout controleur utilisateur

---
+++ src/controleur/Utilisateur.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace wishlist\controleur;
+use wishlist\models\Utilisateur as Uti;
+use wishlist\vue\UtilisateurVue as vUti;
+
+class Utilisateur
+{
+
+    public function registerForm() {
+
+        $v = new vUti();
+        $v->registerForm();
+
+    }
+
+
+    public function createUser(string $nom, string $password) {
+
+        $utilisateur = new vUti();
+        $utilisateur->nom = $nom;
+        $utilisateur->password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+        $utilisateur->save();
+    }
+
+
+    public function authenticateUser(string $nom, string $password) : Uti{
+
+        $uti = Uti::where('nom', '=', $nom)->first();
+
+        if (! is_null($uti)) {
+
+            if (password_verify($password, $uti->password))
+                return $uti;
+
+        }
+
+
+    }
+
+}
