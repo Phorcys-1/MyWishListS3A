e1615df8de51549e9ce90ea159b8f73c440a50f5
Jules <jfrancois968@gmail.com>
2021-01-06 15:40:00 +0100

    faute frappe

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $db->addConnection([
     'host' => 'root',
     'database' => 'wish',
     'username' => 'wish',
-    'passwaord' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => ''
