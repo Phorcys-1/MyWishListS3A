e5884e641976113fd7faa0f3e5ef210ee5f46d7f
Rodolphe Drouet <ID631909@outlook.com>
2021-01-20 20:56:20 +0100

    ajout commentaires

--- src/controleur/ItemControleur.php
+++ src/controleur/ItemControleur.php
@@ -12,14 +12,31 @@ use wishlist\modele\Utilisateur as Utl;
 use wishlist\modele\Item as Itm;
 use wishlist\vue\ItemVue as vItm;
 
-
+/**
+ * Class ItemControleur
+ * @package wishlist\controleur
+ */
 
 class ItemControleur {
     protected $liste;
 
+    /**
+     * ItemControleur constructor.
+     * @param $pName
+     * constructeur
+     */
+
     public function __construct($pName){
         $this->liste=Lst::where('token', '-',$pName)->first();
     }
+
+    /**
+     * @param Request $rq
+     * @param Response $rs
+     * @param array $args
+     * @return Response
+     * methode qui cree un item
+     */
 
     function itemCreation(Request $rq, Response $rs, array $args) : Response{
         $item = new Item();
@@ -39,11 +56,27 @@ class ItemControleur {
         return $rs;
     }
 
+    /**
+     * @param Request $rq
+     * @param Response $rs
+     * @param int $id
+     * @return Response
+     * methode qui affiche un item selon son id
+     */
+
     function afficherItem(Request $rq, Response $rs,int $id) : Response{
         $item= Item::where('liste_id','-',$this->no)->where('id','-',$id)->first();
         $v = new ItemVue($item, $this->c);
         $rs->getBody()->write($v->render(2));
     }
+
+    /**
+     * @param Request $rq
+     * @param Response $rs
+     * @param int $id
+     * @return Response
+     * methode qui supprime un item selon son id
+     */
 
     function supprimerItem(Request $rq, Response $rs, int $id): Response {
         $idItem = filter_var($id,FILTER_SANITIZE_NUMBER_INT);
